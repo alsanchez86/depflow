46381350f1e62e062a4ceab2ba2af9a8ea809326
asanchez86 <angel.sanchez@innofis.com>
2017-05-12 15:36:41 +0200

    add first boostrap component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,50 @@
-// Basic imports
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 
 // Routing
 import { AppRoutingModule } from './app.routing.module';
 
 // Components
 import { AppComponent } from './app.component';
+import { 
+  NgbdButtonsRadio 
+} from './components';
 
 // Layouts
-import { HeaderComponent, SidebarComponent } from './layouts';
+import { 
+  HeaderComponent, 
+  SidebarComponent 
+} from './layouts';
 
 // Pages
-import { NotFoundComponent, HomeComponent } from './pages';
+import { 
+  NotFoundComponent, 
+  HomeComponent 
+} from './pages';
 
 // Meta Decorator
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    NgbModule
+  ],
   declarations: [
     AppComponent,
+    NgbdButtonsRadio,
     
     HeaderComponent,
     SidebarComponent,
 
     NotFoundComponent,
     HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
-  ],
+  ],  
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/buttons-radio/buttons-radio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngbd-buttons-radio',
+  templateUrl: './buttons-radio.component.html',
+  styleUrls: ['./buttons-radio.component.scss']
+})
+
+export class NgbdButtonsRadio {
+  model = 1;
+}
