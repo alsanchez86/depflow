49259e748d9dd8f6b11b1a160b45af70af4b01cd
asanchez86 <angel.sanchez@innofis.com>
2017-06-09 15:18:34 +0200

    update filters and pipes

--- src/app/components/restaurant/restaurant.component.ts
+++ src/app/components/restaurant/restaurant.component.ts
@@ -7,6 +7,9 @@ import { Restaurant } from '../../classes';
 // Services
 import { RestaurantService } from '../../services';
 
+// Extra imports
+import * as _ from 'underscore';
+
 @Component({
   selector: 'restaurant-component',
   templateUrl: './restaurant.component.html',
@@ -14,9 +17,11 @@ import { RestaurantService } from '../../services';
 })
 
 export class RestaurantComponent {
-  public restaurants: Restaurant[]; // array of restaurants
   // public selectedRestaurant: Restaurant; // object restaurant
-  public options: object[];
+  public restaurants: Restaurant[];  
+  public filters: object[];
+  public options: object[];  
+  public order: any;
 
   constructor(
     private restaurantService: RestaurantService
@@ -24,21 +29,48 @@ export class RestaurantComponent {
 
   }
 
-  ngOnInit(){
-    this.getRestaurants();    
+  ngOnInit(){   
+    // filters
+    this.filters = [{
+      value: 1,
+      text: "1 Mesa"
+    },
+    {          
+      value: 2,
+      text: "2 Mesas"
+    }];
+
+    // options
     this.options = [{
       value: "DESC",
-      active: true
+      active: false
     },
     {          
       value: "ASC",
-      active: false
+      active: true
     }];
+    
+    // order (active option)    
+    this.order = _.chain(this.options)
+                  .filter(function(option) { return option["active"]; })
+                  .map(function(option){ return option["value"]; })                  
+                  .value();   
+
+    /*
+      Parece ser que como las funciones de underscore tardan un poco en ejecutarse, se renderiza antes el ngFor de restaurantes en la vista y no toma bien el valor del filtro this.order
+    */ 
+
+    // get Restaurants from API
+    this.getRestaurants();    
   }
 
   getRestaurants(): void {    
     this.restaurantService
       .getAll()
       .then(data => this.restaurants = data);
+  }
+
+  setOrder(event): any {        
+    this.order = event.target.value;
   }
 }
