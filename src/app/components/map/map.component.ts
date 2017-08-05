50b37ef46e96c301a4fed070cf1b1c6f14916e81
alsanchez86 <sanchezpizarro86@gmail.com>
2017-08-05 10:11:47 -0700

    update leaflet map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,6 +15,16 @@ let next = 0;
 
 export class MapComponent {  
   public id: string;
+  private map: object;
+  private apiUrl: string;
+  private container: string;
+  private accessToken: string;
+  private attribution: string;
+  private maxZoom: number;
+  private leafletId: string;
+  private zoom: number;
+  private x: number;
+  private y: number;
 
   constructor(
     private elem: ElementRef
@@ -23,14 +33,34 @@ export class MapComponent {
     this.id = elem.nativeElement.tagName.toLowerCase() + "-" +  next++;
   }
 
-  ngAfterViewInit () {
-    let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+  ngOnInit() {    
+    // this.accessToken  = "?access_token=";
+    this.accessToken  = "";
+    this.maxZoom      = 18;
+    this.leafletId    = 'mapbox.streets';
+    this.container    = "leaflet-map";
+    this.zoom         = 6;
+    this.x            = -5.09;
+    this.y            = 39.505;    
+    this.apiUrl       = 'http://c.tile.osm.org/{z}/{x}/{y}.png' + this.accessToken;
+    this.attribution  = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a   href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';    
+  }
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a   href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      id: 'mapbox.streets',
-      accessToken: 'your.mapbox.access.token'
-    }).addTo(mymap);
+  ngAfterViewInit () {    
+    this.map = L
+                .map(this.container)
+                .setView([
+                  this.y, 
+                  this.x
+                ], this.zoom);
+
+    L
+      .tileLayer(this.apiUrl, {
+        attribution:  this.attribution,
+        maxZoom:      this.maxZoom,
+        id:           this.leafletId,
+        accessToken:  this.accessToken
+      })
+      .addTo(this.map);
   }  
 }
