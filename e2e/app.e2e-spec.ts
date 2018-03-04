0935afc7dd5ab4ee9220d3ad24a3b1603541987c
alsanchez86 <sanchezpizarro86@gmail.com>
2018-03-04 21:41:38 +0100

    update angular to 5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { DepflowPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('depflow App', () => {
-  let page: DepflowPage;
+describe('angular5 App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new DepflowPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,12 @@
-// Modules
-import {
-  enableProdMode
-} from "@angular/core";
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import {
-  platformBrowserDynamic
-} from "@angular/platform-browser-dynamic";
-
-// Components
-import {
-  AppModule
-} from "./app/app.module";
-
-// Enviorements
-import {
-  environment
-} from "./environments/environment";
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-// Start App!
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
